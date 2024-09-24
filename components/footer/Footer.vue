748c92ea1186359d48662e39dd2561181cdf69ad
Ezeh20 <ranter1570@gmail.com>
2024-09-24 16:46:43 +0100

    UI: working on the footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -8,14 +8,14 @@ const isPlayer = ref(trackUri)
 
 
 <template>
-    <footer class="sticky top-[100%] py-[20px]">
+    <footer :class="`sticky top-[100%] py-[30px] bg-primary-color mt-[50px] ${isPlayer && 'pb-[120px]'}`">
         <div class="px-4">
             <div class="flex justify-between items-center">
-                <p class="text-gray-500 text-sm">Copyright © 2024 Echo. All rights reserved.</p>
+                <p class="text-white text-sm">Copyright © 2024 Echo. All rights reserved.</p>
                 <ul class="flex">
                     <li class="mx-4">
                         <a href="https://github.com/Ezeh20/nuxt-ms" target="_blank"
-                            class="text-text-color hover:text-gray-800">View Repo</a>
+                            class="text-white text-sm underline">View Repo</a>
                     </li>
                 </ul>
             </div>

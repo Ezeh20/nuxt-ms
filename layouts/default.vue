dcf87c8d1d745e5ceccf24e3e4fabb95aa68156c
Ezeh20 <ranter1570@gmail.com>
2024-09-25 00:24:52 +0100

    ..

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,20 +20,30 @@ import { useWindowSize } from '@vueuse/core';
 const { width } = useWindowSize();
 
 const layoutClasses = computed(() => [
-    styles.layoutGrid,
-    {
-        [styles.leftActive]: leftBarActive,
-        [styles.leftInactive]: !leftBarActive,
-        [styles.rightActive]: rightBarActive,
-        [styles.rightInactive]: !rightBarActive,
-    }
+  styles.layoutGrid,
+  {
+    [styles.leftActive]: leftBarActive,
+    [styles.leftInactive]: !leftBarActive,
+    [styles.rightActive]: rightBarActive,
+    [styles.rightInactive]: !rightBarActive,
+  }
 ]);
 
 </script>
 
 
 <template>
-    <section :class="[styles.main, layoutClasses]">
+    <section :class="`${styles.main}`" :style="{
+        'grid-template-columns': width < 1024
+            ? '1fr'
+            : leftBarActive && rightBarActive
+                ? '1.75fr 6.5fr 1.75fr'
+                : !leftBarActive && rightBarActive
+                    ? '0fr 6.5fr 1.75fr'
+                    : leftBarActive && !rightBarActive
+                        ? '1.75fr 6.5fr 0fr'
+                        : '0fr 6.5fr 0fr'
+    }">
         <SideBar :class="{ 'sticky top-0 h-screen': width > 1024 }" />
         <section class="bg-background-color w-full flex flex-col h-screen">
             <NavBar class="sticky top-0 z-10" />

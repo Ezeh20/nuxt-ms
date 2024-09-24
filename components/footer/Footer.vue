9f8f20f4a3edfe1cd15d71cbb7a3de94ea6b9699
Ezeh20 <ranter1570@gmail.com>
2024-09-24 11:00:15 +0100

    added footer

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,5 +1,14 @@
 <template>
     <footer class="sticky top-[100%] mb-[70px]"> 
-        footer
+        <div class="container px-4 sm:px-8">
+            <div class="flex justify-between items-center">
+                <p class="text-gray-500 text-sm">Copyright © 2024 Your Website. All rights reserved.</p>
+                <ul class="flex">
+                    <li class="mx-4">
+                        <a href="https://github.com/Ezeh20/nuxt-ms" target="_blank" class="text-text-color hover:text-gray-800">View Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </template>
